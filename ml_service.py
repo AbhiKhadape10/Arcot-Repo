f190d35e9d0232253fec5a9cd2e0801601ac0205
AbhiKhadape10 <83642783+AbhiKhadape10@users.noreply.github.com>
2023-12-03 11:50:10 +0530

    Update ml_service.py

--- ml_service.py
+++ ml_service.py
@@ -1,4 +1,11 @@
-# ml_service.py
+import logging
+
+# Configure basic logging to print messages to the console
+logging.basicConfig(level=logging.INFO)
+
 def predict(data):
+    logging.info("Processing data: %s", data)
     # Your AI/ML logic here
-    return "Prediction result"
+    result = "Prediction result"
+    logging.info("Prediction result: %s", result)
+    return result
