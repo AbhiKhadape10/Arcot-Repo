7304bf59b2ec8148266aa232bb96160dd2294619
AbhiKhadape10 <83642783+AbhiKhadape10@users.noreply.github.com>
2023-12-03 11:48:21 +0530

    Create run_ml_service.py

---
+++ run_ml_service.py
@@ -0,0 +1,6 @@
+from ml_service import predict
+
+if __name__ == "__main__":
+    data = "test_data"  # You can replace this with actual data
+    result = predict(data)
+    print("Final Prediction Result:", result)
